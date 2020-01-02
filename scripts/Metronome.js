d342aec6bf98ee8fba48ac74815efd6942b55717
cgiovano <carlosgiovano@outlook.com>
2020-01-01 23:14:23 -0300

    Improved code

--- scripts/Metronome.js
+++ scripts/Metronome.js
@@ -1,19 +1,19 @@
 class Metronome {
     constructor(beatsPerMinute, beatsPerBar, beatUnit) {
+        const urlList = ['./assets/beep-one.wav', './assets/beep-two.wav'];
+
         this._beatUnit = beatUnit;
         this._beatsPerBar = beatsPerBar;
         this._beatsPerMinute = beatsPerMinute;
-        this.tempoMarkList = ["Grave", "Larghissimo", "Largo", "Larghetto", "Adagio", "Adagietto","Andantino", "Marcia Moderato", "Andante", "Andante Moderato", "Moderato", "Allegro Moderato", "Allegro ma non troppo", "Allegro", "Vivace", "Vivacissimo", "Alegricissimo", "Presto", "Prestissimo"];
-        
-        this.arrayBuffer = [];
+
         this.isRunning = false;
         this.counter = 1;
         this.audioContext = new AudioContext();
         this.bufferSourceNode = undefined;
 
-        this.readFileAsArrayBuffer();
+        let audioDecoder = new AudioDecoder(this.audioContext, urlList);
+        this.arrayBuffer = audioDecoder.decodedAudioDataList;
     }
-
     // GETTERS
     get currentState() {
         return (this.isRunning);
@@ -47,13 +47,16 @@ class Metronome {
     }
 
     /**
-     * @param {Number} "new beatUnit value"
+     * Set the new value for beat interval
+     * @param {Number} newBeatUnitValue new value for BeatUnit
      */
     set beatUnit(newBeatUnitValue) {
         this._beatUnit = newBeatUnitValue;
     }
 
-    // Main
+    /**
+     * Start the metronome main function.
+     */
     task() {
         const m = this;
         let t;
@@ -94,84 +97,5 @@ class Metronome {
         m.isRunning ? m.isRunning = false : m.isRunning = true;
 
         start();
-    }
-
-    getTempoMark() {
-        switch(true) {
-            case (this.beatsPerMinute >= 20 && this.beatsPerMinute < 40):
-                return (this.tempoMarkList[0]);
-                break;
-            case (this.beatsPerMinute >= 40 && this.beatsPerMinute < 45):
-                return (this.tempoMarkList[1]);
-                break;
-            case (this.beatsPerMinute >= 45 && this.beatsPerMinute < 50):
-                return (this.tempoMarkList[2]);
-                break;
-            case (this.beatsPerMinute >= 50 && this.beatsPerMinute < 55):
-                return (this.tempoMarkList[3]);
-                break;
-            case (this.beatsPerMinute >= 55 && this.beatsPerMinute < 65):
-                return (this.tempoMarkList[4]);
-                break;
-            case (this.beatsPerMinute >= 65 && this.beatsPerMinute < 70):
-                return (this.tempoMarkList[5]);
-                break;
-            case (this.beatsPerMinute >= 70 && this.beatsPerMinute < 108):
-                return (this.tempoMarkList[8]);
-                break;
-            case (this.beatsPerMinute >= 108 && this.beatsPerMinute < 112):
-                return (this.tempoMarkList[10]);
-                break;
-            case (this.beatsPerMinute >= 112 && this.beatsPerMinute < 116):
-                return (this.tempoMarkList[11]);
-                break;
-            case (this.beatsPerMinute >= 116 && this.beatsPerMinute < 120):
-                return (this.tempoMarkList[12]);
-                break;
-            case (this.beatsPerMinute >= 120 && this.beatsPerMinute < 140):
-                return (this.tempoMarkList[13]);
-                break;
-            case (this.beatsPerMinute >= 140 && this.beatsPerMinute < 160):
-                return (this.tempoMarkList[14]);
-                break;
-            case (this.beatsPerMinute >= 160 && this.beatsPerMinute < 170):
-                return (this.tempoMarkList[15]);
-                break;
-            case (this.beatsPerMinute >= 170 && this.beatsPerMinute < 180):
-                return (this.tempoMarkList[16]);
-                break;
-            case (this.beatsPerMinute >= 180 && this.beatsPerMinute < 200):
-                return (this.tempoMarkList[17]);
-                break;
-            case (this.beatsPerMinute >= 200):
-                return (this.tempoMarkList[18]);
-                break;
-            default:
-                return ("unknown tempo mark");
-                break;
-        }
-    }
-
-    // Load Sound Sources
-    readFileAsArrayBuffer() {
-        let urlList = ['./assets/beep-one.wav', './assets/beep-two.wav'];
-
-        try {
-            for (let i = 0; i < 2; i++) {
-                let request = new XMLHttpRequest();
-                request.open('GET', urlList[i], true);
-                request.responseType = 'arraybuffer';
-
-                request.onload = () => this.audioContext.decodeAudioData(request.response, (buffer) => this.arrayBuffer[i] = buffer, (error) => console.log(error));
-
-                request.send();
-            }
-
-            //DEBUG
-            console.log("Sound sources were loaded succesfully!");
-        }
-        catch (error) {
-            console.error("wasn't possible load sound sources!" + error);
-        }
     }
 }
