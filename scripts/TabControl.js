fb3c71dc39f815056daf479ed4c15d62586ff77a
cgiovano <carlosgiovano@outlook.com>
2020-03-15 10:35:26 -0300

    .

---
+++ scripts/TabControl.js
@@ -0,0 +1,11 @@
+function hello(tabname) {
+    let tabcontent = document.getElementsByClassName('tabcontent');
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+
+    document.getElementById(tabname).style.display = 'block';
+
+    console.log("hello" + tabname);
+}
