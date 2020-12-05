3e35f1b06a06f7f80f54a7dcaead5f9ead490374
Carlos Giovano <carlosgiovano@outlook.com>
2020-12-05 19:01:18 -0300

    Updated tuner.js
    improved code that precise the frequency and pitch.

--- scripts/Tuner.js
+++ scripts/Tuner.js
@@ -62,16 +62,19 @@ function autoCorrelate(sampleRate, dataBuffer) {
 
     let bestOffset = 0;
     let smallestDifference = Number.POSITIVE_INFINITY;
+    let lastDifference = Number.POSITIVE_INFINITY;
+    let foundGoodDifference = false;
+    var differences = new Array(MAX_SAMPLES);
     let rms = getRMS(dataBuffer);
 
     // Check the level of the signal. If we don't have enough signal we will return -1.
-    if (rms < 0.1) {
+    if (rms < 0.01) {
         return (-1);
     }
 
     // Correlate the data in audio is not compare a half signal size with itself (delayed)
     // We must get the average difference and then we get the correlation
-    for (let offset = 1; offset < MAX_SAMPLES; offset++) {
+    for (let offset = 1; offset <= MAX_SAMPLES; offset++) {
         let difference = 0;
 
         for (let i = 0; i < MAX_SAMPLES; i++) {
@@ -79,18 +82,32 @@ function autoCorrelate(sampleRate, dataBuffer) {
         }
         // Averaged or correlated difference. Is given by ifference summing divided by half of buffer size.
         //let correlation = 1 - (differenceSum / MAX_SAMPLES);
-        difference /= MAX_SAMPLES;
+        difference = difference / MAX_SAMPLES;
+        differences[offset] = difference;
 
-        if (difference < 0.1) {
+        if (difference < 0.1 && difference < lastDifference) {
+            foundGoodDifference = true;
+
             if (difference < smallestDifference) {
                 smallestDifference = difference;
                 bestOffset = offset;  
             }
+        }
+        else if (foundGoodDifference) {
+            var antiAliasedDifference = lerp(differences[bestOffset - 1], differences[bestOffset + 1], differences[bestOffset]);
 
-            if (bestOffset > 1) {
-                return (sampleRate / bestOffset);
+            let res = sampleRate / (bestOffset + (antiAliasedDifference * 12));  //12 is the number of chromatic notes;
+
+            if (isNaN(res) == false) {
+                return(res);
             }
         }
+
+        lastDifference = difference;
+    }
+
+    if (bestOffset > 1) {
+        return (sampleRate / bestOffset);
     }
 }
 
@@ -116,4 +133,15 @@ function frequencyFromNoteNumber( note ) {
 
 function centsOffFromPitch( frequency, note ) {
 	return (Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) ));
+}
+
+// método impreciso para calcular v = v1 quando t = 1 "floating-point arithimetic error" 
+// Essa forma pode ser utilizada em hardware que possui intruções "Multiply-Add"
+/*function lerp(v0, v1, t) {
+    return v0 + t*(v1-v0);
+}*/
+  
+// Método preciso que garante que v = v1 quando t = 1.
+function lerp(v0, v1, t) {
+    return (1-t)*v0 + t*v1;
 }
